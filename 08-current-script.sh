195ae893719aadf43fd5acb6104b640cc5019c8c
ArunkumarThumati <arundevops100@gmail.com>
2024-05-02 05:26:10 -0400

    newshell

--- 08-current-script.sh
+++ 08-current-script.sh
@@ -5,5 +5,5 @@ course="Devops from current script"
 echo "Before the calling current script: $course"
 echo "PID of Current script: $$"
 
-sourse ./09-other-script.sh
+source ./09-other-script.sh
 echo " After calling other script: $course"
